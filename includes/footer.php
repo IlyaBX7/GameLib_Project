3c684c0fae3cbbc1b2acfcb533306e62ac70eab3
Ilya Basko <rrrrabigor@gmail.com>
2026-05-13 15:23:06 +0300

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -53,7 +53,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <script src="<?php echo BASE_URL; ?>js/main.js?v=<?php echo time(); ?>"></script>
 <?php if (isset($page_js)): ?>
-    <script src="<?php echo resolve_url($page_js); ?>"></script>
+    <script src="<?php echo resolve_url($page_js); ?>?v=<?php echo time(); ?>"></script>
 <?php endif; ?>
 </body>
 </html>
