6db6959eae1310cb47eaf4ddead859aaf922152e
Ilya Basko <rrrrabigor@gmail.com>
2026-05-13 15:23:23 +0300

    Update login.php

--- auth/login.php
+++ auth/login.php
@@ -41,6 +41,11 @@ require_once '../includes/header.php';
             <p class="mb-0"><?php echo $errors[0]; ?></p>
         </div>
     <?php endif; ?>
+    <?php if (isset($_GET['reset']) && $_GET['reset'] === 'success'): ?>
+        <div class="alert alert-success">
+            <p class="mb-0">Пароль успішно змінено. Ви можете увійти з новим паролем.</p>
+        </div>
+    <?php endif; ?>
 
     <form action="login.php" method="POST" class="bg-dark-green p-4 rounded">
         <div class="mb-3">
@@ -49,14 +54,22 @@ require_once '../includes/header.php';
         </div>
         <div class="mb-3">
             <label for="password" class="form-label">Пароль</label>
-            <input type="password" class="form-control" id="password" name="password" required>
+            <div class="input-group">
+                <input type="password" class="form-control" id="password" name="password" required>
+                <button class="btn btn-outline-secondary" type="button" id="togglePassword">
+                    <svg id="eyeIcon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+                        <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
+                        <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
+                    </svg>
+                </button>
+            </div>
         </div>
         <button type="submit" class="btn btn-success w-100">Увійти</button>
     </form>
 
     <?php
     $googleClientID = '78698731201-o8ildr7r3bqdfh0peoor9o0e77eut636.apps.googleusercontent.com';
-    $googleRedirectUri = 'http://localhost/gamelib/google_callback.php'; 
+    $googleRedirectUri = 'http://' . $_SERVER['HTTP_HOST'] . '/auth/google_callback.php'; 
     $googleLoginUrl = 'https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=' . $googleClientID . '&redirect_uri=' . urlencode($googleRedirectUri) . '&scope=email%20profile';
     ?>
 
@@ -68,9 +81,27 @@ require_once '../includes/header.php';
         </a>
     </div>
 
-    <p class="text-center mt-3">
+    <p class="text-center mt-3 mb-1">
         Немає акаунту? <a href="register.php" class="text-accent">Зареєструватися</a>
     </p>
+    <p class="text-center">
+        <a href="reset_password.php" class="text-accent">Забули пароль?</a>
+    </p>
 </div>
+
+<script>
+document.getElementById('togglePassword').addEventListener('click', function (e) {
+    const password = document.getElementById('password');
+    const eyeIcon = document.getElementById('eyeIcon');
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    
+    if (type === 'password') {
+        eyeIcon.innerHTML = '<path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/><path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>';
+    } else {
+        eyeIcon.innerHTML = '<path d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7.028 7.028 0 0 0-2.79.588l.77.771A5.944 5.944 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.134 13.134 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755l-.976-.976z"/><path d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829l.822.822zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829z"/><path d="M3.35 5.47c-.18.16-.353.322-.518.487A13.134 13.134 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7.029 7.029 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12-.708.708z"/>';
+    }
+});
+</script>
 
 <?php require_once '../includes/footer.php'; ?>
